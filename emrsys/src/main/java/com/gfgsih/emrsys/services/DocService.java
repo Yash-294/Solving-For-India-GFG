a173761939edbf561ec3b78baa44dd469a9dbea5
Yash Shinde <90593348+Yash-294@users.noreply.github.com>
2023-04-13 00:46:24 +0530

    Delete emrsys directory

--- emrsys/src/main/java/com/gfgsih/emrsys/services/DocService.java
+++
@@ -1,9 +0,0 @@
-package com.gfgsih.emrsys.services;
-
-import com.gfgsih.emrsys.dto.DoctorDTO;
-import com.gfgsih.emrsys.entities.Doctor;
-
-public interface DocService {
-	void saveDoc(DoctorDTO docdto);
-	Doctor findByUserid(String doc_id);
-}
